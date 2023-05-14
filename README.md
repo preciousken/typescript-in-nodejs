7153fce8f6eb920735820377428b3760f513c28e
preciousken <pkehinde27@gmail.com>
2023-05-14 09:50:07 +0100

    completed

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # typescript-in-nodejs
+- $ tsc (for compiling into javascript)
+- $ npm run dev (for running nodemon)
--- build/index.js
+++ build/index.js
@@ -1,1 +1,25 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.get("/", (req, res) => {
+    try {
+        res.json({
+            status: true,
+            message: " You are welcome comrade"
+        });
+    }
+    catch (error) {
+        console.log(error);
+        res.json({
+            status: false,
+            message: "You've got some errors"
+        });
+    }
+});
+app.listen("5000", () => {
+    console.log('server running! ');
+});
